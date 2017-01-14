56e9b8c75bdb4345718f0d5beb7cc1bf3cca629e
tranh <tranh@10.89.60.29>
2017-01-14 12:33:06 -0500

    Empty robot project

---
+++ src/Robot.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <IterativeRobot.h>
+
+class Robot: public frc::IterativeRobot {
+public:
+	void RobotInit() {
+
+	}
+
+	void AutonomousInit() override {
+
+	}
+
+	void AutonomousPeriodic() {
+
+	}
+
+	void TeleopInit() {
+
+	}
+
+	void TeleopPeriodic() {
+
+	}
+
+	void TestPeriodic() {
+
+	}
+
+private:
+
+};
+
+START_ROBOT_CLASS(Robot)
