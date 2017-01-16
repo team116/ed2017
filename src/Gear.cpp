5bc486525679fc87025410ddd310deb32fe0915e
Emina Ermin <emina.ermin24@gmail.com>
2017-01-16 17:20:09 -0500

    Servo stuff

--- src/Gear.cpp
+++ src/Gear.cpp
@@ -9,10 +9,21 @@
 #include <Gear.h>
 
 Gear* Gear::INSTANCE = nullptr;
+const float Gear::CLOSED_ANGLE = 0.0;
+const float Gear::OPEN_ANGLE = 30.0;
+
 
 Gear::Gear() {
 	// TODO Auto-generated constructor stub
+	servo = new Servo(4);
+}
 
+void Gear::open() {
+	servo->SetAngle(OPEN_ANGLE);
+}
+
+void Gear::close() {
+	servo->SetAngle(CLOSED_ANGLE);
 }
 
 void Gear::process() {
--- src/Gear.h
+++ src/Gear.h
@@ -4,6 +4,7 @@
  *  Created on: Jan 15, 2017
  *      Author: Will
  */
+#include <Servo.h>
 
 #ifndef SRC_GEAR_H_
 #define SRC_GEAR_H_
@@ -12,10 +13,15 @@ class Gear {
 public:
 	static Gear* getInstance();
 	void process();
+	void open();
+	void close();
 
 private:
 	Gear();
 	static Gear* INSTANCE;
+	static const float CLOSED_ANGLE;
+	static const float OPEN_ANGLE;
+	Servo* servo;
 };
 
 #endif /* SRC_GEAR_H_ */
