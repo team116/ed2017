e512f706692969fb4ee650b3560f6663e104c4eb
Emina Ermin <emina.ermin24@gmail.com>
2017-01-16 16:58:23 -0500

    finished basic mobility stuff

--- src/Mobility.cpp
+++ src/Mobility.cpp
@@ -10,11 +10,25 @@
 Mobility* Mobility::INSTANCE = nullptr;
 
 Mobility::Mobility() {
-
+	front_left = new VictorSP(0);
+	front_right = new VictorSP(1);
+	back_right = new VictorSP(2);
+	back_left = new VictorSP(3);
 }
 
 void Mobility::process() {
 
+}
+
+void Mobility::setLeft(float speed) {
+	front_left->Set(speed);
+	back_left->Set(speed);
+
+}
+
+void Mobility::setRight(float speed) {
+	front_right->Set(speed);
+	back_right->Set(speed);
 }
 
 Mobility* Mobility::getInstance()
--- src/Mobility.h
+++ src/Mobility.h
@@ -4,7 +4,7 @@
  *  Created on: Jan 15, 2017
  *      Author: Will
  */
-
+#include <VictorSP.h>
 #ifndef SRC_MOBILITY_H_
 #define SRC_MOBILITY_H_
 
@@ -12,10 +12,16 @@ class Mobility {
 public:
 	static Mobility* getInstance();
 	void process();
+	void setLeft(float speed);
+	void setRight(float speed);
 
 private:
 	Mobility();
 	static Mobility* INSTANCE;
+	VictorSP* front_left;
+	VictorSP* front_right;
+	VictorSP* back_right;
+	VictorSP* back_left;
 };
 
 #endif /* SRC_MOBILITY_H_ */
