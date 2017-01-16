e2e5e6136ae4ad9df993ee1bc7f1296f948b244f
Emina Ermin <emina.ermin24@gmail.com>
2017-01-16 17:41:24 -0500

    Climber stuff

--- src/Climber.cpp
+++ src/Climber.cpp
@@ -11,11 +11,23 @@ Climber* Climber::INSTANCE = nullptr;
 
 Climber::Climber() {
 	// TODO Auto-generated constructor stub
-
+	climber = new VictorSP(5);
 }
 
 void Climber::process() {
 
+}
+
+void Climber::climbUp(float speed) {
+	climber->Set(1.0);
+}
+
+void Climber::climbDown(float speed) {
+	climber->Set(-1.0);
+}
+
+void Climber::climbStop(float speed) {
+	climber->Set(0.0);
 }
 
 Climber* Climber::getInstance()
--- src/Climber.h
+++ src/Climber.h
@@ -4,7 +4,7 @@
  *  Created on: Jan 15, 2017
  *      Author: Will
  */
-
+#include <VictorSP.h>
 #ifndef SRC_CLIMBER_H_
 #define SRC_CLIMBER_H_
 
@@ -13,10 +13,14 @@ public:
 	static Climber* getInstance();
 
 	void process();
+	void climbUp(float speed);
+	void climbDown(float speed);
+	void climbStop(float speed);
 
 private:
 	Climber();
 	static Climber* INSTANCE;
+	VictorSP* climber;
 };
 
 #endif /* SRC_CLIMBER_H_ */
