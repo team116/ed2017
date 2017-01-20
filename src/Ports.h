cb1c92c60a0222f5623626edae9187ed9ce86139
Will Carty <cartycrabber@gmail.com>
2017-01-19 19:25:48 -0500

    They've changed the ports already RIP

--- src/Ports.h
+++ src/Ports.h
@@ -16,17 +16,18 @@ namespace RobotPorts
 	const unsigned int MOTOR_RIGHT_FRONT = 1;
 	const unsigned int MOTOR_LEFT_BACK = 2;
 	const unsigned int MOTOR_RIGHT_BACK = 3;
-
 	const unsigned int MOTOR_SHOOTER_AZIMUTH = 4;
 	const unsigned int MOTOR_SHOOTER_FEEDER = 5;
 	const unsigned int MOTOR_INTAKE = 6;
-	const unsigned int MOTOR_CLIMBER = 7;
-	const unsigned int MOTOR_BLENDER = 8;
+	const unsigned int SERVO_GEAR_RIGHT = 8;
+	const unsigned int SERVO_GEAR_LEFT = 7;
+	const unsigned int SERVO_CONVERTER = 9;
 
 	//navX Motors
-	const unsigned int SERVO_GEAR_RIGHT = 10;//N0
-	const unsigned int SERVO_GEAR_LEFT = 11;//N1
-	const unsigned int SERVO_CONVERTER = 12;//N2
+	//Possible gear motor?
+	const unsigned int MOTOR_GEAR = 10;//N0
+	const unsigned int MOTOR_BLENDER = 11;//N1
+	const unsigned int MOTOR_CLIMBER = 12;//N2
 
 	//CAN Motors
 	const unsigned int MOTOR_SHOOTER_WHEEL = 1;
