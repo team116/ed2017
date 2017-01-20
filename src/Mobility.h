abee69757b806b8b5d7f6305689833968b48a8fb
Emina Ermin <emina.ermin24@gmail.com>
2017-01-19 20:07:17 -0500

    Issue #4

--- src/Mobility.h
+++ src/Mobility.h
@@ -19,17 +19,31 @@ public:
 	void setRight(float speed);
 	void setAngle(float angle);
 	void setStraightSpeed(float speed);
+	void setTurningDegrees(float degrees);
 
 private:
 	Mobility();
 	static Mobility* INSTANCE;
+
+	void processDriveStraight();
+	void processTurningDegrees();
+
 	VictorSP* front_left;
 	VictorSP* front_right;
 	VictorSP* back_right;
 	VictorSP* back_left;
+
 	AnalogGyro* gyro;
+
 	float target_angle;
 	float straight_speed;
+
+	float acceptable_error;
+	float degrees;
+	float starting_degree;
+	float target_degree;
+	float current_angle;
+	float degree_range;
 };
 
 #endif /* SRC_MOBILITY_H_ */
