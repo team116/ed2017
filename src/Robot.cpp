e3e3e7d38fee6ea32057a59b95a5dbb6708b77db
Will Carty <cartycrabber@gmail.com>
2017-01-30 18:52:10 -0500

    Added try/catches to RobotInit and periodic to help with debugging

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -30,38 +30,95 @@ private:
 public:
 	void RobotInit() {
 		try {
-			autonomous = Autonomous::getInstance();
-			climber = Climber::getInstance();
-			gear = Gear::getInstance();
-			intake = Intake::getInstance();
-			mobility = Mobility::getInstance();
-			oi = OI::getInstance();
-			shooter = Shooter::getInstance();
-			vision = Vision::getInstance();
-			diagnostics = Diagnostics::getInstance();
 			log = Log::getInstance();
-		} catch(std::exception e) {
-			frc::DriverStation::ReportError("Flagrant System Error (RobotInit): ");
-			frc::DriverStation::ReportError(e.what());
+		} catch(std::exception* e) {
+			frc::DriverStation::ReportError("Error initializing Log");
+			frc::DriverStation::ReportError(e->what());
+		}
+
+		try {
+			autonomous = Autonomous::getInstance();
+		} catch(std::exception* e) {
+			log->write(Log::ERROR_LEVEL, "Error initializing Autonomous\n%s", e->what());
+		}
+
+		try {
+			climber = Climber::getInstance();
+		} catch(std::exception* e) {
+			log->write(Log::ERROR_LEVEL, "Error initializing Climber\n%s", e->what());
+		}
+
+		try {
+			gear = Gear::getInstance();
+		} catch(std::exception* e) {
+			log->write(Log::ERROR_LEVEL, "Error initializing Gear\n%s", e->what());
+		}
+
+		try {
+			intake = Intake::getInstance();
+		} catch(std::exception* e) {
+			log->write(Log::ERROR_LEVEL, "Error initializing Intake\n%s", e->what());
+		}
+
+		try {
+			mobility = Mobility::getInstance();
+		} catch(std::exception* e) {
+			log->write(Log::ERROR_LEVEL, "Error initializing Mobility\n%s", e->what());
+		}
+
+		try {
+			oi = OI::getInstance();
+		} catch(std::exception* e) {
+			log->write(Log::ERROR_LEVEL, "Error initializing OI\n%s", e->what());
+		}
+
+		try {
+			shooter = Shooter::getInstance();
+		} catch(std::exception* e) {
+			log->write(Log::ERROR_LEVEL, "Error initializing Shooter\n%s", e->what());
+		}
+
+		try {
+			vision = Vision::getInstance();
+		} catch(std::exception* e) {
+			log->write(Log::ERROR_LEVEL, "Error initializing Vision\n%s", e->what());
+		}
+
+		try {
+			diagnostics = Diagnostics::getInstance();
+		} catch(std::exception* e) {
+			log->write(Log::ERROR_LEVEL, "Error initializing Diagnostics\n%s", e->what());
 		}
 	}
 
 	void AutonomousInit() override {
-		mobility->StartDriveDistance(24);
+		try {
+			mobility->StartDriveDistance(24);
+		} catch(std::exception* e) {
+			log->write(Log::ERROR_LEVEL, "Error in AutonomousInit\n%s", e->what());
+		}
 	}
 
 	void AutonomousPeriodic() {
-		autonomous->process();
-		mobility->process();
-		climber->process();
-		gear->process();
-		intake->process();
-		shooter->process();
-		diagnostics->process();
+		try {
+			autonomous->process();
+			mobility->process();
+			climber->process();
+			gear->process();
+			intake->process();
+			shooter->process();
+			diagnostics->process();
+		} catch(std::exception* e) {
+			log->write(Log::ERROR_LEVEL, "Error in AutonomousPeriodic\n%s", e->what());
+		}
 	}
 
 	void TeleopInit() {
+		try {
 
+		} catch(std::exception* e) {
+			log->write(Log::ERROR_LEVEL, "Error in TeleopInit\n%s", e->what());
+		}
 	}
 
 	void TeleopPeriodic() {
@@ -73,17 +130,25 @@ public:
 			intake->process();
 			shooter->process();
 			diagnostics->process();
-		} catch(std::exception e) {
-			frc::DriverStation::ReportError("Flagrant System Error (TeleopPeriodic):");
-			frc::DriverStation::ReportError(e.what());
+		} catch(std::exception* e) {
+			log->write(Log::ERROR_LEVEL, "Error in TeleopPeriodic\n%s", e->what());
 		}
 	}
 
 	void TestInit() {
+		try {
+
+		} catch(std::exception* e) {
+			log->write(Log::ERROR_LEVEL, "Error in TestInit\n%s", e->what());
+		}
 	}
 
 	void TestPeriodic() {
-		TeleopPeriodic();
+		try {
+			TeleopPeriodic();
+		} catch(std::exception* e) {
+			log->write(Log::ERROR_LEVEL, "Error in TestPeriodic\n%s", e->what());
+		}
 	}
 };
 
