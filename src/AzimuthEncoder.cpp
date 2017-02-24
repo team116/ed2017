92d6437eb0c00214553ed082374ff05139bbc3a2
Will Carty <cartycrabber@gmail.com>
2017-02-24 18:25:38 -0500

    AzimuthEncoder extends PIDSource

--- src/AzimuthEncoder.cpp
+++ src/AzimuthEncoder.cpp
@@ -7,6 +7,7 @@
 
 #include <AzimuthEncoder.h>
 #include <Ports.h>
+#include <DriverStation.h>
 
 const float UPPER_ANGLE = 310;
 const float LOWER_ANGLE = 50;
@@ -16,15 +17,22 @@ AzimuthEncoder::AzimuthEncoder() {
 	// TODO Auto-generated constructor stub
 	prev_angle = 0;
 	full_rotations = 0;
-	pot = new frc::AnalogPotentiometer(RobotPorts::AZIMUTH_ENCODER, 360, 0);
+	//pot = new frc::AnalogPotentiometer(RobotPorts::AZIMUTH_ENCODER, 360, 0);
+
+	angle_offset = 0.0;
+
+	reset();
 }
 
 void AzimuthEncoder::process() {
+	frc::DriverStation::ReportError("Prev: " + std::to_string(prev_angle) + " Now: " + std::to_string(pot->Get()));
 	if ((prev_angle > UPPER_ANGLE) && (pot->Get() < LOWER_ANGLE)) {
+		frc::DriverStation::ReportError("Adding rotation");
 		full_rotations++;
 	}
 
 	if ((prev_angle < LOWER_ANGLE) && (pot->Get() > UPPER_ANGLE)) {
+		frc::DriverStation::ReportError("Subtracting rotation");
 		full_rotations--;
 	}
 
@@ -33,5 +41,13 @@ void AzimuthEncoder::process() {
 
 
 float AzimuthEncoder::getAngle() {
-	return ((((full_rotations/ROT_PER_360) * 360) + (pot->Get() / 5)));
+	return ((full_rotations / ROT_PER_360 * 360) + (pot->Get() / 5)) - angle_offset;
+}
+
+void AzimuthEncoder::reset() {
+	angle_offset += getAngle();
+}
+
+double AzimuthEncoder::PIDGet() {
+	return getAngle();
 }
