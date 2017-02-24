d64dca2d8f9953495af7d33f2d625391f977065b
Will Carty <cartycrabber@gmail.com>
2017-02-24 18:31:20 -0500

    uncommented driver station stuff in routine

--- src/AutoPlays/Routine.cpp
+++ src/AutoPlays/Routine.cpp
@@ -6,33 +6,33 @@
  */
 
 #include <AutoPlays/Routine.h>
-#include <DriverStation.h>
+//#include <DriverStation.h>
 
 void Routine::addAction(Action* action) {
 	actions.push(action);
-	frc::DriverStation::ReportError("adding action");
+	//frc::DriverStation::ReportError("adding action");
 }
 
 void Routine::start() {
 	timeout_timer = new frc::Timer();
 	if(!actions.empty()) {
-		frc::DriverStation::ReportError("starting routine");
+		//frc::DriverStation::ReportError("starting routine");
 		current_action = actions.front();
 		actions.pop();
 		current_action->start();
 		timeout_timer->Start();
 	}
 	else {
-		frc::DriverStation::ReportError("nothing to start");
+		//frc::DriverStation::ReportError("nothing to start");
 		current_action = nullptr;
 	}
 }
 
 void Routine::process() {
 	if(current_action != nullptr) {
-		frc::DriverStation::ReportError("timeout: " + std::to_string(current_action->getTimeout()) + " current: " + std::to_string(timeout_timer->Get()));
+		//frc::DriverStation::ReportError("timeout: " + std::to_string(current_action->getTimeout()) + " current: " + std::to_string(timeout_timer->Get()));
 		bool timed_out = (current_action->getTimeout() > 0.0) && (timeout_timer->Get() >= current_action->getTimeout());
-		frc::DriverStation::ReportError("Timed out: " + std::to_string(timed_out));
+		//frc::DriverStation::ReportError("Timed out: " + std::to_string(timed_out));
 		if(current_action->isFinished() || timed_out) {
 			current_action->end();
 			if(!actions.empty()) {
