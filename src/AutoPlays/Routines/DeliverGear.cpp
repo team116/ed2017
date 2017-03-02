ee1c0f380c26fb314283bdd43c29df40f00e598f
Will Carty <cartycrabber@gmail.com>
2017-03-01 21:03:45 -0500

    Tweaked DeliverGear rightforward numbers. still need to tweak leftforward

--- src/AutoPlays/Routines/DeliverGear.cpp
+++ src/AutoPlays/Routines/DeliverGear.cpp
@@ -19,7 +19,7 @@ DeliverGear::DeliverGear(Utils::AutoLocation location) {
 			addAction(new TurnDegrees(15.2));
 			addAction(new DriveStraight(69.0, 0.25));
 			addAction(new DropGear());
-			addAction(new DriveStraight(-12.0, 0.25));
+			addAction(new DriveStraight(-30.0, 0.25));
 			addAction(new CloseGearHolder());
 		}
 		else if (location == Utils::AutoLocation::MiddleForward) {
@@ -27,15 +27,16 @@ DeliverGear::DeliverGear(Utils::AutoLocation location) {
 			addAction(new DriveStraight(60.0, 0.75));
 			addAction(new DriveStraight(15.0, 0.25));
 			addAction(new DropGear());
-			addAction(new DriveStraight(-12.0, 0.25));
+			addAction(new DriveStraight(-24.0, 0.25));
 			addAction(new CloseGearHolder());
 		}
 		else if (location == Utils::AutoLocation::RightForward) {
-			addAction(new DriveStraight(130.3, 0.75));
-			addAction(new TurnDegrees(-15.2));
-			addAction(new DriveStraight(66.5, 0.25));
+			addAction(new DriveStraight(70.0, 0.75));
+			addAction(new TurnDegrees(-60.0));
+			addAction(new DriveStraight(40.5, 0.75));
+			addAction(new DriveStraight(30.5, 0.25));
 			addAction(new DropGear());
-			addAction(new DriveStraight(-12.0, 0.25));
+			addAction(new DriveStraight(-24.0, 0.25));
 			addAction(new CloseGearHolder());
 		}
 	//}
