ad06724d823a7e682483a48a3e5fca5b4b0ab8e2
Will Carty <cartycrabber@gmail.com>
2017-03-04 15:19:55 -0500

    Made log save location persistent

--- src/Log.cpp
+++ src/Log.cpp
@@ -110,7 +110,7 @@ char* Log::generateLogFilename()
 	int file_num = 0;
 
 	while(true){
-		sprintf(filename,"/var/volatile/tmp/%s%d%s",base,file_num,ext);
+		sprintf(filename,"/home/lvuser/%s%d%s",base,file_num,ext);
 
 		previous_log_file = fopen(filename,"r");
 		if(!previous_log_file){
